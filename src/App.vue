f658149f497d29f2725cbf29a7d1ef2d5608a559
walner <git@walner.cl>
2024-03-19 18:11:47 -0600

    v

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li v-if="isLoggedIn()" class="nav-item" :class="{ 'active': $route.path === '/home' }">
+          <li class="nav-item" :class="{ 'active': $route.path === '/home' }">
             <router-link to="/home" class="nav-link" aria-current="page">Mostrar</router-link>
           </li>
-          <li v-if="isLoggedIn()" class="nav-item" :class="{ 'active': $route.path === '/reporte' }">
+          <li class="nav-item" :class="{ 'active': $route.path === '/reporte' }">
             <router-link to="/reporte" class="nav-link">Reporte</router-link>
           </li>
         </ul>
@@ -22,29 +22,17 @@
   </div>
 </template>
 
-<script>
-export default {
-  methods: {
-    isLoggedIn() {
-      // Verificar si hay un token de sesión almacenado
-      const token = localStorage.getItem('token');
-      return !!token; // Devuelve verdadero si hay un token, falso de lo contrario
+<style>
+  .nav-item.active {
+    background-color: #13543b; /* Color de fondo para el elemento de menú activo */
+  }
+
+  /* Media query para pantallas pequeñas */
+  @media (max-width: 992px) {
+    .nav-item.active {
+      background-color: #13543b; /* Cambia el color de fondo en pantallas pequeñas */
+      padding: 5px; /* Ajusta el padding del elemento de menú activo en pantallas pequeñas */
+      width: 80px;
     }
   }
-};
-</script>
-
-<style>
-.nav-item.active {
-  background-color: #13543b; /* Color de fondo para el elemento de menú activo */
-}
-
-/* Media query para pantallas pequeñas */
-@media (max-width: 992px) {
-  .nav-item.active {
-    background-color: #13543b; /* Cambia el color de fondo en pantallas pequeñas */
-    padding: 5px; /* Ajusta el padding del elemento de menú activo en pantallas pequeñas */
-    width: 80px;
-  }
-}
 </style>
